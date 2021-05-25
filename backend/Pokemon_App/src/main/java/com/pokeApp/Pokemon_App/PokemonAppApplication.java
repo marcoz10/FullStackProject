fa8b9acebc5af7ee1722c905778d7ad4a6cb7e84
Marco Zamora <business.marcozamora@gmail.com>
2021-05-25 01:34:47 -0500

    Added Spring component to project.

---
+++ backend/Pokemon_App/src/main/java/com/pokeApp/Pokemon_App/PokemonAppApplication.java
@@ -0,0 +1,13 @@
+package com.pokeApp.Pokemon_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonAppApplication.class, args);
+	}
+
+}
---
+++ backend/Pokemon_App/src/test/java/com/pokeApp/Pokemon_App/PokemonAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokeApp.Pokemon_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
