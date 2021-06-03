f0261fab5f235cf721f1da01e6ccf5b5d8434584
ThaFishDance <chuffman91@gmail.com>
2021-06-03 13:15:20 -0500

    add the chip aka pill for the types

--- frontend/pokemon-app/src/components/Card.js
+++ frontend/pokemon-app/src/components/Card.js
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from "react";
+import TypeChip from "./TypeChip";
 
-function Card({id, name, height, weight, type}) {
-
-    return (
-        <div style={{border: '1px solid red'}}>
-
-            <h4>#{id}: {name}</h4>
-            <p>Height: {height}</p>
-            <p>Weight: {weight}</p>
-            <p>Type: {type}</p>
-        </div>
-    )
+function Card({ id, name, height, weight, type }) {
+  return (
+    <div style={{ border: "1px solid red" }}>
+      <h4>
+        #{id}: {name}
+      </h4>
+      <p>Height: {height}</p>
+      <p>Weight: {weight}</p>
+      <TypeChip label={type} />
+    </div>
+  );
 }
 
-export default Card
+export default Card;
