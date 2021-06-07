44cac6b0e799cfda8575fa092ac7645c8d5c1d99
ThaFishDance <chuffman91@gmail.com>
2021-06-07 16:17:34 -0500

    add bg color

--- frontend/pokemon-app/src/components/Card.js
+++ frontend/pokemon-app/src/components/Card.js
@@ -8,17 +8,17 @@ function Card({ id, name, height, weight, type, type2, image }) {
   }
 
   return (
-    <div className="p-2 m-2 border border-red-500">
+    <div className="p-2 m-2 border border-red-500 bg-red-100">
       <h4 className="text-lg text-center font-press-start">
         #{id}: {capitalize(name)}
       </h4>
       <img className="mx-auto" src={image} alt={name} />
-      <p className="text-sm font-press-start p-1">Height: {height}</p>
-      <p className="text-sm font-press-start p-1">Weight: {weight}</p>
-      <div className="flex justify-center space-x-1">
+      <div className="flex justify-center space-x-1 py-1">
         <TypeChip label={capitalize(type)} />
         {type2 && <TypeChip label={capitalize(type2)} />}
       </div>
+      <p className="text-sm font-press-start p-1">Height: {height}</p>
+      <p className="text-sm font-press-start p-1">Weight: {weight}</p>
     </div>
   );
 }
